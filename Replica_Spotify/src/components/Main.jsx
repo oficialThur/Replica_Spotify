2b385eb35a69c466808aeccca92974679e8d046d
oficialThur <oficialthurhenrique@gmail.com>
2025-05-05 23:35:26 -0300

    aula_1_completa

---
+++ Replica_Spotify/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <h2>Artistas Populares</h2>
+        <a className='main_link' href="">Mostrar mais</a>
+    </div>
+  )
+}
+
+export default Main
