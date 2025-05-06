e32be25cd73c9a9028aba1cb2c1abdb71a6afca6
oficialThur <oficialthurhenrique@gmail.com>
2025-05-06 10:22:57 -0300

    aula_1_dia_2

--- Replica_Spotify/src/components/Main.jsx
+++ Replica_Spotify/src/components/Main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 
 const Main = () => {
   return (
     <div className='main'>
-        <h2>Artistas Populares</h2>
-        <a className='main_link' href="">Mostrar mais</a>
+      <div className='item-list'>
+
+        <div className='item-list_header'>
+          <h2>Artistas Populares</h2>
+          <a className='item-list_link' href="">Mostrar mais</a>
+        </div>
+        <div className='single-item'>
+          <div className='single-item_div-image'>
+            <img className='single-item_image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem Henrique & Juliano" />
+          </div>
+          <FontAwesomeIcon className='single-item_icon' icon={faCirclePlay} />
+          <div className='single-item_text'>
+            <p className='single-item_title'>Henrique & Juliano</p>
+            <p className='single-item_type'>Artista</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
